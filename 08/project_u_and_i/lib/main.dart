127d72998adc00539e2b85d704d38c33ee59935b
Deon <urban.seonbi.deon@gmail.com>
2023-03-15 00:54:34 +0900

    08, u and i, '테마 적용해보기' 남음

---
+++ 08/project_u_and_i/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:project_u_and_i/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}

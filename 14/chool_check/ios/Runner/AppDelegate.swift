8083d0708c10632432a8e8287957b0dbcc916763
Deon <urban.seonbi.deon@gmail.com>
2023-03-15 04:38:39 +0900

    14, 오늘도 출근, '카메라 위치 애니메이션으로 이동하기' 남음

--- 14/chool_check/ios/Runner/AppDelegate.swift
+++ 14/chool_check/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDM_fN2bEFiNBFzgsgxpmS7BWLxt30XPZY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
